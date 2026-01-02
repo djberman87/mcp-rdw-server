871f2732dea71b863dae698602e508575b759f7c
Dirk-Jan Berman <djberman87@gmail.com>
2026-01-03 00:02:32 +0100

    Fix: Final .NET build issue on Windows and version bump to 1.2.7

--- nodejs/index.js
+++ nodejs/index.js
@@ -12,7 +12,7 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 const server = new Server(
   {
     name: "rdw-server",
-    version: "1.2.6",
+    version: "1.2.7",
   },
   {
     capabilities: {
